8d5a00c0afa5b95d8843a9b1bf0ae7b3929642c9
thotenn <josenriqueee@gmail.com>
2022-04-14 16:29:31 -0400

    react 18 upds

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-if (process.env.NODE_ENV !== 'production') {require('./init.css')};
-import {App} from './App';
+import {createRoot} from 'react-dom/client';
+if (process.env.NODE_ENV !== 'production') { require('./init.css') };
+import { App } from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
